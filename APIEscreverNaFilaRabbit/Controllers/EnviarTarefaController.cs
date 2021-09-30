c4637d278168295d14eb6d0b30c362bdb200d324
EMMANUEL EGITO OLIVEIRA CANDEIRA MARINHO <e_egito@hotmail.com>
2021-09-30 19:04:34 -0300

    Adicionar arquivos de projeto.

---
+++ APIEscreverNaFilaRabbit/Controllers/EnviarTarefaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace APIEscreverNaFilaRabbit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnviarTarefaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult InsertTarefa([FromBody] Tarefa tarefa)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using(var connection = factory.CreateConnection())
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "minhafila1",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    var message = JsonSerializer.Serialize(tarefa);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "minhafila1",
+                        basicProperties: properties,
+                        body: body);
+                }
+                return Ok("Tarefa cadastrada na fila");
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ APIEscreverNaFilaRabbit/Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIEscreverNaFilaRabbit
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Prioridade { get; set; }
+
+    }
+}
